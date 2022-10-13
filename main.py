fe957790da930fec368bbb45d1a56486daa5ce30
yuyu <hcsd7b34@gmail.com>
2022-10-14 08:08:45 +0900

    Add: パスワード確認画面を実装

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import hashlib
 import os
 import wx
 
@@ -38,6 +39,7 @@ class LockFrame(wx.Frame):
 
     sizer.Add(wx.StaticText(panel, label='パスワード', style=wx.TE_LEFT), (0, 1), flag=wx.EXPAND | wx.ALIGN_CENTER)
     password_textctrl = wx.TextCtrl(panel)
+    self.password_textctrl = password_textctrl
     sizer.Add(password_textctrl, (0, 2), flag=wx.EXPAND | wx.ALL, border=10)
 
     sizer.AddGrowableCol(2)
@@ -46,11 +48,47 @@ class LockFrame(wx.Frame):
     top_sizer.Add(btn_sizer, flag=wx.EXPAND | wx.ALL, border=10)
 
     lock_btn = wx.Button(panel, wx.ID_OK, label='ロック')
+    lock_btn.Bind(wx.EVT_BUTTON, lambda event: self.lock())
     lock_btn.SetDefault()
     btn_sizer.AddButton(lock_btn)
     
     cancel_btn = wx.Button(panel, wx.ID_CANCEL, label='キャンセル')
     cancel_btn.Bind(wx.EVT_BUTTON, lambda event: self.Close())
+    btn_sizer.AddButton(cancel_btn)
+    
+    btn_sizer.Realize()
+    
+  def lock(self):
+    hashed_password = hashlib.sha256(str(self.password_textctrl.GetValue()).encode()).digest()
+    
+    result = ConfirmPasswordDialog(hashed_password).ShowModal()
+    if (result == wx.ID_CANCEL): self.Close()
+    elif (result == wx.ID_NO): wx.MessageBox('パスワードが異なっています')
+    
+class ConfirmPasswordDialog(wx.Dialog):
+  def __init__(self, hashed_password):
+    self.hashed_password = hashed_password
+    
+    super().__init__(None, title='パスワードの確認', size=(300, 125))
+    self.SetBackgroundColour('white')
+    self.SetWindowStyle(wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
+
+    sizer = wx.BoxSizer(wx.VERTICAL)
+    self.SetSizer(sizer)
+    
+    password_textctrl = wx.TextCtrl(self, style=wx.TE_PASSWORD)
+    sizer.Add(password_textctrl, flag=wx.EXPAND | wx.ALL, border=10)
+    
+    btn_sizer = wx.StdDialogButtonSizer()
+    sizer.Add(btn_sizer, flag=wx.EXPAND | wx.ALL, border=10)
+
+    lock_btn = wx.Button(self, wx.ID_OK, label='OK')
+    lock_btn.Bind(wx.EVT_BUTTON, lambda event: 
+      self.EndModal(wx.ID_YES) if hashlib.sha256(str(password_textctrl.GetValue()).encode()).digest() == self.hashed_password else self.EndModal(wx.ID_NO))
+    lock_btn.SetDefault()
+    btn_sizer.AddButton(lock_btn)
+    
+    cancel_btn = wx.Button(self, wx.ID_CANCEL, label='キャンセル')
     btn_sizer.AddButton(cancel_btn)
     
     btn_sizer.Realize()
