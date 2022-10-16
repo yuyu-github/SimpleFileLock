f95651c206cff559c009d59f7c4f9065f9a483bd
yuyu <hcsd7b34@gmail.com>
2022-10-16 10:46:12 +0900

    Add: アンロックを実装

--- main.py
+++ main.py
@@ -36,12 +36,41 @@ def encrypt(data: bytes, key: bytes, algorithm: EncryptionAlgorithm = DEFAULT_EN
       cipher = aes.encrypt(data)
       return (cipher, iv)
     
-def lock(path, encryption_key, confirm_meta):
+def decrypt(cipher: bytes, key: bytes, decryption_meta: bytes, algorithm: EncryptionAlgorithm = DEFAULT_ENCRYPTION_ALGORITHM):
+  match algorithm:
+    case EncryptionAlgorithm.AES_CTR:
+      iv = decryption_meta
+      ctr = Counter.new(AES.block_size * 8, initial_value=int.from_bytes(iv, 'big'))
+      aes = AES.new(key, AES.MODE_CTR, counter=ctr)
+      data = aes.decrypt(cipher)
+      return data
+    
+def lock(path, encryption_key):
+  confirm_meta = encryption_key
+  
   newpath = re.sub(r'\.runsfl$', '.sfl', path) if path.endswith('.runsfl') else path + '.sfl'
   with open(path, 'rb') as org, open(newpath, 'wb') as locked:
     (cipher, decryption_meta) = encrypt(len(confirm_meta).to_bytes(2, 'big') + confirm_meta + org.read(), encryption_key)
     locked.write(DEFALUT_HASH_ALGORITHM.value.to_bytes(1, 'big') + DEFAULT_ENCRYPTION_ALGORITHM.value.to_bytes(1, 'big') + \
       len(decryption_meta).to_bytes(2, 'big') + decryption_meta + cipher)
+
+def unlock(path, decryption_key):
+  newpath = re.sub(r'\.sfl$', '', path)
+  with open(path, 'rb') as locked, open(newpath, 'wb') as org:
+    locked.seek(1)
+    encryption_algorithm = EncryptionAlgorithm(int.from_bytes(locked.read(1), 'big'))
+    decryption_meta_len = int.from_bytes(locked.read(2), 'big')
+    decryption_meta = locked.read(decryption_meta_len)
+    cipher = locked.read()
+    
+    data = decrypt(cipher, decryption_key, decryption_meta, encryption_algorithm)
+    confirm_meta_len = int.from_bytes(data[0:2], 'big')
+    confirm_meta = data[2:2+confirm_meta_len]
+
+    if confirm_meta == decryption_key:
+      org.write(data[2+confirm_meta_len:])
+    else:
+      wx.MessageBox('パスワードが間違っています', 'エラー')
   
 def open_file(paths: list[str]):
   lock_files = []
@@ -101,8 +130,8 @@ class StartFrame(wx.Frame):
 
     if dialog.ShowModal() == wx.ID_OK:
       paths = dialog.GetPaths()
-      if len(paths) >= 1:
-        pass
+      for path in paths:
+        UnlockFrame(path).Show(True)
         
   def on_edit_file_button_pressed(self, event):
     dialog = wx.FileDialog(self, 'ファイルの選択')
@@ -161,11 +190,61 @@ class LockFrame(wx.Frame):
       try:
         for path in self.paths:
           encryption_key = hashed_password
-          confirm_meta = encryption_key
-          lock(path, encryption_key, confirm_meta)
+          lock(path, encryption_key)
       except Exception as e:
         wx.MessageBox(str(e), 'エラーが発生しました')
       finally: self.Close()
+      
+class UnlockFrame(wx.Frame):
+  def __init__(self, path: str):
+    self.path = path
+    
+    name = os.path.basename(path)
+    super().__init__(None, title=name + 'をアンロック', size=(400, 150))
+    self.SetBackgroundColour('white')
+    self.SetWindowStyle(wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
+    
+    panel = wx.Panel(self)
+    top_sizer = wx.BoxSizer(wx.VERTICAL)
+    panel.SetSizer(top_sizer)
+    
+    sizer = wx.GridBagSizer()
+    top_sizer.Add(sizer, 1, flag=wx.EXPAND)
+
+    sizer.Add(wx.StaticText(panel, label='パスワード', style=wx.TE_LEFT), (0, 1), flag=wx.EXPAND | wx.ALIGN_CENTER)
+    password_textctrl = wx.TextCtrl(panel, style=wx.TE_PASSWORD)
+    self.password_textctrl = password_textctrl
+    sizer.Add(password_textctrl, (0, 2), flag=wx.EXPAND | wx.ALL, border=10)
+
+    sizer.AddGrowableCol(2)
+    
+    btn_sizer = wx.StdDialogButtonSizer()
+    top_sizer.Add(btn_sizer, flag=wx.EXPAND | wx.ALL, border=10)
+
+    lock_btn = wx.Button(panel, wx.ID_OK, label='アンロック')
+    lock_btn.Bind(wx.EVT_BUTTON, lambda event: self.unlock())
+    lock_btn.SetDefault()
+    btn_sizer.AddButton(lock_btn)
+    
+    cancel_btn = wx.Button(panel, wx.ID_CANCEL, label='キャンセル')
+    cancel_btn.Bind(wx.EVT_BUTTON, lambda event: self.Close())
+    btn_sizer.AddButton(cancel_btn)
+    
+    btn_sizer.Realize()
+    
+  def unlock(self):    
+    try:
+      f = open(self.path, 'rb')
+      f.seek(0)
+      hash_algorithm = HashAlgorithm(int.from_bytes(f.read(1), 'big'))
+      f.close()
+      hashed_password = hash(str(self.password_textctrl.GetValue()).encode(), hash_algorithm).to_bytes()
+    
+      decryption_key = hashed_password
+      unlock(self.path, decryption_key)
+    except Exception as e:
+      wx.MessageBox(str(e), 'エラーが発生しました')
+    finally: self.Close()
     
 class ConfirmPasswordDialog(wx.Dialog):
   def __init__(self, hashed_password: bytes):
